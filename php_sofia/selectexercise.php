b40a8c64451fe1c3747ee924f98a9b966662df4a
Sofku800 <sofia.rots@student.hamk.fi>
2024-02-15 16:09:40 +0200

    up

--- php_sofia/selectexercise.php
+++ php_sofia/selectexercise.php
@@ -38,6 +38,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Select</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="/style/ryhmaliikunta_Sofia_Rots.css">
 </head>
 <body>
 <header>
--- php_sofia/varauslomake.php
+++ php_sofia/varauslomake.php
@@ -64,7 +64,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>varauslomake</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="/style/ryhmaliikunta_Sofia_Rots.css">
 </head>
 <header>
         <ul>
@@ -90,7 +90,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>
     <div>
         <h2>Täytä varaajan tiedot</h2>
-        <form action="./varauslomake.php" method="post">
+        <form class="form-horizontal" action="./varauslomake.php" method="post">
             <label for="fname">First Name:</label>
             <input type="text" name="fname" value=""><br>
 
